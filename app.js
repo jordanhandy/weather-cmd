b6aa4f41bbf2e23a014c87a7579f7ff122bdc5bf
Jordan Handy <handy.jordan@gmail.com>
2021-04-15 23:16:01 -0400

    ES6 destructuring

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const forecast = require("./utils/forecast");
 
 // Callback chaining
 // use the output of 'data' as input to the second callback function (forecast)
-geocode(process.argv[2],(error,data) => {
+geocode(process.argv[2],(error,{latitude,longitude,placeName}={}) => {
   if(process.argv[2]===undefined){
     return console.log(chalk.inverse.red("ERROR. NO ADDRESS PROVIDED"));
   }
   if(error){
     return console.log(error);
   }
-  forecast(data.latitude,data.longitude,(error, forecastData) => {
+  forecast(latitude, longitude,(error, forecastData) => {
     if (error){
       return console.log(error);
     }
-    console.log(chalk.inverse.blue(data.placeName));
+    console.log(chalk.inverse.blue(placeName));
     console.log(forecastData);
   })
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,20 +1,20 @@
 const request = require("postman-request");
 const forecast = (lat,lon,callback) => {
     const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=`+lat+","+lon;
-    request({url:url,json:true},(error,response) => {
+    request({url,json:true},(error,{body}) => {
       if(error){
         callback("Unable to connect to service",undefined);
-      }else if(response.body.error){
+      }else if(body.error){
         callback("Unable to find location.  Please try again.",undefined);
       } else{
         callback(undefined,"FORECAST: " +
-                      response.body.current.weather_descriptions[0] +
+                      body.current.weather_descriptions[0] +
                       "\nIt is currently " +
-                      response.body.current.temperature +
+                      body.current.temperature +
                       " degrees outside.\nThere is currently a " +
-                      response.body.current.precip +
+                      body.current.precip +
                       " percent chance of rain.\nIt feels like " +
-                      response.body.current.feelslike +
+                      body.current.feelslike +
                       " degrees outside."
         )
       }
