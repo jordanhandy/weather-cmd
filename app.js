8c69fb2095d4606f91aad78f577fdcedbd3e4c4f
Jordan Handy <handy.jordan@gmail.com>
2021-04-15 22:26:09 -0400

    fix weather api, refactor

--- app.js
+++ app.js
@@ -3,43 +3,16 @@
 require("dotenv").config();
 const chalk = require("chalk");
 // request for API calls
-const request = require("postman-request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
-// define API URLs
-// weatherstack
-// const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=New_York`;
-// request(
-//   {
-//     // define URL, and force JSON
-//     url: url,
-//     json: true,
-//   },
-//   // an error and response comes back
-//   (error, response) => {
-//     if (error) {
-//       chalk.inverse.red(console.log("Unable to connect to weather service"));
-//     }else if(response.body.error){
-//       chalk.inverse.red(console.log("Unable to find location"));
-//     } 
-//     else {
-//       const data = response.body;
-//       console.log(
-//         "FORECAST: " +
-//           data.current.weather_descriptions[0] +
-//           "\nIt is currently " +
-//           data.current.temperature +
-//           " degrees outside.\nThere is currently a " +
-//           data.current.precip +
-//           " percent chance of rain.\nIt feels like " +
-//           data.current.feelslike +
-//           " degrees outside."
-//       );
-//     }
-//   }
-// );
 
-geocode("Ajax",(error, data) => {
+
+geocode("boston",(error,data) => {
+  console.log("Error",error);
+  console.log("Data",data);
+})
+
+forecast(40.7831,-73.9712,(error, data) => {
   console.log("Error",error);
   console.log('Data',data);
 })
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,17 +1,22 @@
 const request = require("postman-request");
 const forecast = (lat,lon,callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=`+encodeURI(lat)+","+encodeURI(lon);
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=`+lat+","+lon;
     request({url:url,json:true},(error,response) => {
       if(error){
         callback("Unable to connect to service",undefined);
       }else if(response.body.error){
         callback("Unable to find location.  Please try again.",undefined);
       } else{
-        callback(undefined,{
-          temp:response.body.current.temperature,
-          precip:response.body.current.precip,
-          feelslike:response.body.current.feelslike
-        })
+        callback(undefined,"FORECAST: " +
+                      response.body.current.weather_descriptions[0] +
+                      "\nIt is currently " +
+                      response.body.current.temperature +
+                      " degrees outside.\nThere is currently a " +
+                      response.body.current.precip +
+                      " percent chance of rain.\nIt feels like " +
+                      response.body.current.feelslike +
+                      " degrees outside."
+        )
       }
     })
   }
